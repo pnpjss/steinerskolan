b1d4d50678cd089fe7b8731ac2840c55337d839d
Trilisen <jonmcg01@gmail.com>
2021-10-06 14:49:49 +0200

    Start on nav

--- html/wp-content/themes/steinerskolan/header.php
+++ html/wp-content/themes/steinerskolan/header.php
@@ -11,13 +11,20 @@
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
 
-    <?php $menuItems = wp_get_nav_menu_items("main-menu"); ?>
+    <?php $menuItems = wp_get_nav_menu_items("Navigation menu"); ?>
 
-    <nav>
+    <nav class="nav-container">
+        <div class="logo"><img src="/wp-content/uploads/steinerskolan-logo.jpg" alt="Logo"></div>
+        <div>
         <?php $currentPageId = $wp_query->queried_object_id;
         foreach ($menuItems as $item) : ?>
             <a class="nav-link<?= $item->object_id == $currentPageId ? ' active' : '' ?>" href="<?= $item->url; ?>">
                 <?= $item->title; ?>
             </a>
         <?php endforeach; ?>
+        </div>
+        <div class="search-container">
+            <div class="stylish-line">|</div>
+            <div class="search-box">SEARCH</div>
+        </div>
     </nav>
