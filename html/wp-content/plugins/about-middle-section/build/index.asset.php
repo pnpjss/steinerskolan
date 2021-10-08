b61c542cf9ff2018ebf5ce97a525bc704a7862a7
pnpjss <pnpjss@github.com>
2021-10-08 14:19:35 +0100

    About page images and scaling

--- html/wp-content/plugins/about-middle-section/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ce2174f843ebbc6c1acf16e54250426a');
