923c18879fb6432255f31e115c113e16f309446a
pnpjss <pnpjss@github.com>
2021-10-11 16:47:41 +0100

    Footer init

--- html/wp-content/themes/steinerskolan/footer.php
+++ html/wp-content/themes/steinerskolan/footer.php
@@ -1,4 +1,44 @@
+<!-- <script src="<?= get_theme_file_uri('./assets/kanye.jpg') ?>" async></script> -->
 <?php wp_footer(); ?>
+<footer>
+   <div class="grid-item-1">
+       <a href="/home">
+       <img  class="logo-image" src="<?= get_theme_file_uri('./assets/steiner-logo.svg') ?>" alt="logo image footer">
+    </a>
+    <p>Att väcka nyfikenhet, förundran och lusten att lära sig</p>
+    <div class="social-media-links">
+      <a href="/">
+      <img  class="fb-image" src="<?= get_theme_file_uri('./assets/fb-logo.svg') ?>" alt="link to facebook">
+    </a>
+    <a href="/">
+        <img  class="insta-image" src="<?= get_theme_file_uri('./assets/insta-logo.svg') ?>" alt="link to instagram">
+    </a>
+    <a href="/">
+        <img  class="mail-image" src="<?= get_theme_file_uri('./assets/mail-logo.svg') ?>" alt="link to mail">
+    </a>
+
+    </div>
+   </div>
+   <div class="grid-item-2">
+       <ul>
+           <li class="list-item-1">
+               <a href="">Kontakta oss</li></a> 
+           <li class="list-item-2">Expedition: 031214632</li>
+           <li class="list-item-3">Vaktmästare: 0705-1150 98</li>
+           <li class="list-item-4">E-post: rudolf@steinerskolan.se</li>
+       </ul>
+       <hr>
+    </div>
+    <div class="grid-item-3">
+        <ul>
+            <li><a href="">Arbeta med oss</a></li>
+            <li><a href="">Kalendarium</a></li>
+            <li><a href="">Hitta oss</a></li>
+            <li></li>
+        </ul>
+        <hr class="hr hr-bottom">
+   </div>
+</footer>
 </body>
 
 </html>
