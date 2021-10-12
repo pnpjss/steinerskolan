def3d47ad2448472c619f46178dfdab92034ee54
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-10-12 13:36:50 +0200

    edit page.php

--- html/wp-content/themes/steinerskolan/page.php
+++ html/wp-content/themes/steinerskolan/page.php
@@ -1,14 +1,13 @@
 <?php get_header(); ?>
 
-<div class="container">
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
 
-            <?php the_content(); ?>
+        <?php the_content(); ?>
 
-        <?php endwhile; ?>
-    <?php endif; ?>
-</div>
+    <?php endwhile; ?>
+<?php endif; ?>
+
 
 
 <?php get_footer(); ?>
