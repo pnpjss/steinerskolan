b5a0ffe7832236e24a3f67110c2b6525141ffe13
Trilisen <jonmcg01@gmail.com>
2021-10-13 10:53:55 +0200

    Application top done

---
+++ html/wp-content/plugins/apply-top/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '20f05da7764c708f6349f99bc94b3628');
---
+++ html/wp-content/plugins/apply-top/src/edit.js
@@ -0,0 +1,96 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { __ } from "@wordpress/i18n";
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
+ */
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import "./editor.scss";
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+
+import {
+	MediaUpload,
+	MediaUploadCheck,
+	useBlockProps,
+	RichText,
+} from "@wordpress/block-editor";
+import { Button } from "@wordpress/components";
+
+export default function Edit({ attributes, setAttributes }) {
+	const { image } = attributes;
+	return (
+		<div className="apply-top-content" {...useBlockProps()}>
+			<MediaUploadCheck>
+				<MediaUpload
+					onSelect={(media) => setAttributes({ image: media })}
+					allowedTypes={["image"]}
+					value={image ? image.id : ""}
+					render={({ open }) =>
+						image ? (
+							<div className="apply-image">
+								<img src={image.url} width={image.width / 2} />
+
+								<Button
+									onClick={() => setAttributes({ image: "" })}
+									className="button is-small"
+								>
+									Remove
+								</Button>
+							</div>
+						) : (
+							<Button onClick={open} className="open-btn">
+								Upload image
+							</Button>
+						)
+					}
+				/>
+			</MediaUploadCheck>
+			<div className="apply-text">
+				<RichText
+					placeholder={__("write your title here", "apply-top")}
+					tagName={"h1"}
+					value={attributes.title}
+					onChange={(value) => setAttributes({ title: value })}
+				/>
+				<RichText
+					placeholder={__("Paragraph for apply-top", "apply-top")}
+					tagName={"p"}
+					value={attributes.text}
+					onChange={(value) => setAttributes({ text: value })}
+				/>
+				<RichText
+					placeholder={__("Apply link title", "apply-top")}
+					tagName={"a"}
+					value={attributes.applyLinkTitle}
+					onChange={(value) => setAttributes({ applyLinkTitle: value})}
+				/>
+				<RichText
+					placeholder={__("Apply link url", "apply-top")}
+					value={attributes.applyUrl}
+					onChange={(value) => setAttributes({ applyUrl: value})}
+				/>
+			</div>
+		</div>
+	);
+}
